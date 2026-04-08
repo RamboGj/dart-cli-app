094e12a34f4904b4b81114aa2f7e296c9d60b4fe
artemis <jpramboguanabara@hotmail.com>
2026-04-08 11:02:24 -0300

    Command runner

--- dartpedia/cli/bin/cli.dart
+++ dartpedia/cli/bin/cli.dart
@@ -1,39 +1,70 @@
-import 'package:cli/cli.dart' as cli;
 import 'dart:io';
 import 'package:http/http.dart' as http;
+import 'package:command_runner/command_runner.dart';
 
 const version = '0.0.1';
-void main(List<String> arguments) {
-  if (arguments.isEmpty || arguments.first == 'help') {
-    printUsage();
-  } else if (arguments.first == 'version') {
-    // cli.version(version);
-    print('Dartpedia CLI version $version');
-  } else if (arguments.first == 'search') {
-    final inputArgs = arguments.length > 1 ? arguments.sublist(1) : null;
-    searchWikipedia(inputArgs);
-  } else {
-    printUsage();
-  }
+
+void main(List<String> arguments) async {
+  var runner = CommandRunner();
+  await runner.run(arguments);
 }
 
-void searchWikipedia(List<String>? arguments) {
-  final String articleTitle;
+// void main(List<String> arguments) {
+//   if (arguments.isEmpty || arguments.first == 'help') {
+//     printUsage();
+//   } else if (arguments.first == 'version') {
+//     // cli.version(version);
+//     print('Dartpedia CLI version $version');
+//   } else if (arguments.first == 'wikipedia') {
+//     final inputArgs = arguments.length > 1 ? arguments.sublist(1) : null;
+//     searchWikipedia(inputArgs);
+//   } else {
+//     printUsage();
+//   }
+// }
 
-  if (arguments == null || arguments.isEmpty) {
-    print('Please provide an article title.');
-    articleTitle = stdin.readLineSync() ?? '';
-  } else {
-    articleTitle = arguments.join(' ');
-  }
+// void searchWikipedia(List<String>? arguments) async {
+//   final String? articleTitle;
 
-  print('Looking up articles about "$articleTitle". Please wait.');
-  print('Here ya go!');
-  print('(Pretend this is an article about "$articleTitle")');
-}
+//   if (arguments == null || arguments.isEmpty) {
+//     print('Please provide an article title.');
 
-void printUsage() {
-  print(
-    "The following commands are valid: 'help', 'version', 'search <ARTICLE-TITLE>'",
-  );
-}
+//     final inputFromStdin = stdin.readLineSync();
+
+//     if (inputFromStdin == null || inputFromStdin.isEmpty) {
+//       print('No article title provided. Exiting.');
+//       return;
+//     }
+
+//     articleTitle = inputFromStdin;
+//   } else {
+//     articleTitle = arguments.join(' ');
+//   }
+
+//   print('Looking up articles about "$articleTitle". Please wait.');
+
+//   var articleContent = await getWikipediaArticle(articleTitle);
+
+//   print(articleContent);
+// }
+
+// void printUsage() {
+//   print(
+//     "The following commands are valid: 'help', 'version', 'search <ARTICLE-TITLE>'",
+//   );
+// }
+
+// Future<String> getWikipediaArticle(String articleTitle) async {
+//   final url = Uri.https(
+//     'en.wikipedia.org',
+//     '/api/rest_v1/page/summary/$articleTitle',
+//   );
+
+//   final response = await http.get(url);
+
+//   if (response.statusCode == 200) {
+//     return response.body;
+//   }
+
+//   return 'Error: Failed to fetch article "$articleTitle". Status code: ${response.statusCode}';
+// }
